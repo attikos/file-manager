3a15839e1cf0188826da0a0a59f50f8514f2680f
Bylkov Maksim <vuegit@gmail.com>
2022-06-10 01:08:46 +0400

    fix: os --cpus

--- src/modules/os.js
+++ src/modules/os.js
@@ -1,9 +1,18 @@
 import os from 'os'
 
+const getCpusInfo = () => {
+    const cpus = os.cpus()
+
+    return cpus.map(x => {
+        const {model, speed} = x
+
+        return {model, speed}
+    })
+}
 export default async function([arg]) {
     const argsMap = {
         '--EOL': () => JSON.stringify(os.EOL),
-        '--cpus': () => os.cpus(),
+        '--cpus': () => getCpusInfo(),
         '--homedir': () => os.homedir(),
         '--username': () => os.userInfo().username,
         '--architecture': () => os.arch(),
@@ -13,7 +22,7 @@ export default async function([arg]) {
         throw Error(`Bad argument: ${arg}`)
     }
 
-    const res = argsMap[arg]();
+    const res = argsMap[arg]()
 
-    console.log(res);
+    console.log(res)
 }
